136b19930b5e9a2e53f2262ee7075eb38d5fe795
mwawrzyniak <mwawrzyniak@unistra.fr>
2023-09-30 15:42:04 +0200

    feat(main): add start of implementation of todo command options

--- ToDoList/src/main.cpp
+++ ToDoList/src/main.cpp
@@ -1,8 +1,60 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
+/*o [name,
+desc, dc, df, prio, id]*/
+void help() {
+    cout << "Commandes:\n";
+    cout << "./todo <option>\n\n";
+    cout << "OPTIONS:\n";
+    cout << "\t-c (Démarre la création d'une nouvelle tache)\n";
+    cout << "\t-r (Affiche la liste des taches)\n";
+    cout << "\t\t-o <name,desc, dc, df, prio, id> (Trier les taches)\n";
+    cout << "\t-r <ID> (Affiche la tache avec l'ID donnée)\n";
+    cout << "\t-u <ID> (Modifier la tache avec l'ID donnée)\n";
+    cout << "\t-d <ID> (Supprimer la tache avec l'ID donnée)\n";
+    cout << "\t-l <file> (Rensigner le fichier qu'on veut charger, consulter ou modifier)\n";
+    cout << "\t-s <file> (Renseigner le fichier sur lequel sauvegarder la liste des tâches après modifications)\n";
+}
 
-int main() {
-    cout << "ToDoList !" << endl;
+int main(int argc, char *argv[]) {
+    if(argc < 2) {
+        help();
+        return 0;
+    }
+    if(string(argv[1]) == "-c") {
+        cout << "Creation d'une nouvelle tache\n";
+        // TODO
+    } else if(string(argv[1]) == "-r") {
+        if(argc == 2) {
+            cout << "Affiche la liste des taches\n";
+            // TODO
+        } else {
+            if(argc == 4) {
+                cout << "Affiche la liste des taches trié par " << argv[3] << "\n";
+                // TODO
+            } else{
+                help();
+                return 0;
+            }
+        }
+    } else if(string(argv[1]) == "-u") {
+        cout << "Modification de la tache avec l'ID donnée\n";
+        // TODO
+    } else if(string(argv[1]) == "-d") {
+        cout << "Suppression de la tache avec l'ID donnée\n";
+        // TODO
+    } else if(string(argv[1]) == "-l") {
+        cout << "Rensigner le fichier qu'on veut charger, consulter ou modifier\n";
+        // TODO
+    } else if(string(argv[1]) == "-s") {
+        cout << "Renseigner le fichier sur lequel sauvegarder la liste des tâches après modifications\n";
+        // TODO
+    } else {
+        help();
+        return 0;
+    }
+
     return 0;
 }
