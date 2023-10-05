0a5911889d869d7f84d26e326f2fae46544e11dd
mwawrzyniak <mwawrzyniak@unistra.fr>
2023-10-05 18:38:28 +0200

    feat(display-tasks): add todo.h to implement base functions

---
+++ ToDoList/includes/todo.h
@@ -0,0 +1,15 @@
+#ifndef TODO_H
+#define TODO_H
+
+#include "task.h"
+#include "sort.h"
+#include "json_helper.h"
+#include <iostream>
+#include <string>
+#include <limits>
+#include <fstream>
+
+
+void displayTasks(ifstream &file, string typeSort);
+
+#endif
