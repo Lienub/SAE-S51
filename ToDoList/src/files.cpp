88ac42b062393472f71b05d81d558dacc447fa21
mwawrzyniak <mwawrzyniak@unistra.fr>
2023-10-06 16:42:06 +0200

    docs(load-files): add comments for doxygen

--- ToDoList/src/files.cpp
+++ ToDoList/src/files.cpp
@@ -4,6 +4,18 @@
 #include "../includes/task.h"
 #include "../includes/files.h"
 
+/**
+ * @brief get loaded file based on file type
+ *
+ * This function allows you to retrieve the name of 
+ * the file which contains spots depending on the 
+ * file type
+ *
+ * @param filenameLoadFiles The name of the file which contains the loaded files.
+ * @param type The type of file (out or in)
+ * @return The name of the loaded file
+ */
+
 string loadFilename(string filenameLoadFiles, string type)
 {
     fstream fileLoad(filenameLoadFiles, fstream::in);
@@ -26,6 +38,19 @@ string loadFilename(string filenameLoadFiles, string type)
     return filename;
 }
 
+/**
+ * @brief save loaded file based on file type
+ *
+ * This function allows you to save the name of 
+ * the file which contains spots depending on the 
+ * file type
+ *
+ * @param loadFile The name of the loaded file which is saved
+ * @param filename The name of the file which contains the loaded files.
+ * @param type The type of file (out or in)
+ * @return 0 if the file is saved, 1 if the file does not exist
+ */
+ 
 int saveFilenameLoaded(string loadFile, string filename, string type)
 {
     fstream fstreamFile(filename, fstream::in);
