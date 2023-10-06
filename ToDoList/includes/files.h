73fad07f56077b52067a9a63cc176307e3c14a11
mwawrzyniak <mwawrzyniak@unistra.fr>
2023-10-06 12:29:13 +0200

    feat(load-files): implementation save filename floaded in load files

---
+++ ToDoList/includes/files.h
@@ -0,0 +1,11 @@
+#ifndef FILES_H
+#define FILES_H
+
+using namespace std;
+
+#include <string>
+#include <iostream>
+
+int saveFilenameLoaded(string loadFile, string filename, string type);
+
+#endif
---
+++ ToDoList/src/file.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "../includes/task.h"
+#include "../includes/files.h"
+
+string loadFilename(string filenameLoadFiles, string type)
+{
+    fstream fileLoad(filenameLoadFiles, fstream::in);
+    if (!fileLoad.is_open())
+    {
+        cout << "Erreur lors de la lecture du fichier\n";
+        return "";
+    }
+    string line;
+    string filename = "";
+    while (getline(fileLoad, line))
+    {
+        if (line.substr(0, line.find('|')) == type)
+        {
+            fileLoad.close();
+            filename = line.substr(line.find('|'), line.length());
+        }
+    }
+    fileLoad.close();
+    return filename;
+}
+
+int saveFilenameLoaded(string loadFile, string filename, string type)
+{
+    fstream fstreamFile(filename, fstream::in);
+    if (!fstreamFile.is_open())
+    {
+        cout << "Votre fichier n'existe pas\n";
+        fstreamFile.close();
+        return 1;
+    }
+    else
+    {
+        ofstream outputLoadedFile("load_files_temp.txt");
+        string line;
+        while (getline(fstreamFile, line))
+        {
+            if (line.substr(0, line.find('|')) != type)
+            {
+                outputLoadedFile << line << "\n";
+            }
+        }
+        outputLoadedFile << type + "|" + loadFile << "\n";
+        outputLoadedFile.close();
+        fstreamFile.close();
+        remove(filename.c_str());
+        rename("load_files_temp.txt", filename.c_str());
+        fstreamFile.close();
+    }
+    return 0;
+}
