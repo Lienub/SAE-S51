d9d96703c7f4b65b509479bfc9733d5dc4cc43fd
mwawrzyniak <mwawrzyniak@unistra.fr>
2023-10-07 07:58:15 +0200

    fix(delete-task): remove unused gitignore, logs folder and add delete_task_test unit test with example data test file

---
+++ ToDoList/tests/todo/delete_task_test.cpp
@@ -0,0 +1,51 @@
+#define CATCH_CONFIG_MAIN
+#include "../catch2/catch.hpp"
+#include "../../includes/json_helper.h"
+#include "../../includes/task.h"
+#include "../../includes/todo.h"
+#include <sstream>
+#include <fstream>
+
+TEST_CASE("Delete task from JSON file", "[deleteTask]")
+{
+    json jsonContent = R"([
+        {
+            "id": 1,
+            "name": "Courses",
+            "description": "faire les courses",
+            "priority": "HIGH",
+            "start_date": "2020-01-01",
+            "end_date": "2020-01-02"
+        },
+        {
+            "id": 2,
+            "name": "Devoirs",
+            "description": "Faire S51",
+            "priority": "HIGH",
+            "start_date": "2020-01-01",
+            "end_date": "2020-01-02"
+        },
+        {
+            "id": 3,
+            "name": "Projets",
+            "description": "Faire S51",
+            "priority": "HIGH",
+            "start_date": "2020-01-01",
+            "end_date": "2020-01-02"
+        }
+    ])"_json;
+    ofstream oFile("../data/tasks_test.json");
+    oFile << jsonContent.dump(4);
+    oFile.close();
+
+    ifstream iFile("../data/tasks_test.json");
+    int taskIdToDelete = 2;
+    json result = deleteTask(iFile, taskIdToDelete);
+    iFile.close();
+    REQUIRE(result.size() == 2);
+    REQUIRE(result[0]["id"] == 1);
+    REQUIRE(result[1]["id"] == 3);
+
+    REQUIRE(result[0]["name"] == "Courses");
+    REQUIRE(result[1]["name"] == "Projets");
+}
